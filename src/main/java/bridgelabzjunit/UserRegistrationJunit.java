8d2eea5484648494a4dd6ff60675cfbdc4faef8c
Shivaleela <shivaleelakatti13@gmail.com>
2023-12-01 15:42:55 +0530

    creating the pattern that clear all the email samples provided samples

--- src/main/java/bridgelabzjunit/UserRegistrationJunit.java
+++ src/main/java/bridgelabzjunit/UserRegistrationJunit.java
@@ -3,6 +3,7 @@ package bridgelabzjunit;
 
 public class UserRegistrationJunit {
 
+	//uc1 creating the first name with valid uppercase first letter and 3 characters//
 	public boolean validateFirstName(String firstName) {
         boolean isValid = firstName.matches("[A-Z][a-z]{3,10}");
         if (isValid) {
@@ -14,6 +15,7 @@ public class UserRegistrationJunit {
         return isValid;
         }
 	
+	//uc2 creating the last name with valid uppercase first letter and 3 characters//
 	public boolean validateLastName(String lastName) {
         boolean isValid = lastName.matches("[A-Z][a-z]{3,10}");
         if (isValid) {
@@ -25,8 +27,10 @@ public class UserRegistrationJunit {
         return isValid;
         }
 	
+	//uc3 creating the email id with valid email format//
+	//uc9 creating the pattern that clear all the email samples provided samples 
 	public boolean validateEmail_id(String email_id) {
-        boolean isValid = email_id.matches("[A-Za-z0-9+_.-]+@gmail\\.com$");
+        boolean isValid = email_id.matches("^[A-Za-z0-9+_.-]+@gmail\\.com$");
         if (isValid) {
             System.out.println("Email is valid");
             
@@ -36,6 +40,7 @@ public class UserRegistrationJunit {
         return isValid;
         }
 	
+	//uc4 creating the phone num with the proper + , country code and 110 digit number//
 	public boolean validatePhone_Number(String phone_num) {
         boolean isValid = phone_num.matches("\\+[0-9]{1,3}[0-9]{10}$");
         if (isValid) {
@@ -47,6 +52,10 @@ public class UserRegistrationJunit {
         return isValid;
         }
 	
+	//uc5 creating the password with min of 8 character
+	//uc6 creating password with atleast one uppercase
+	//uc7 creating password with atleast one numeric character
+	//uc8 creating password with atleast one special character
 	public boolean validatePassword(String password) {
         boolean isValid = password.matches("(?=.*[A-Z])(?=.*[a-z\\d])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
         if (isValid) {
