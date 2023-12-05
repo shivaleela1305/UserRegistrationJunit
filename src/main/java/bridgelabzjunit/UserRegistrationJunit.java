6136c30fcf1be16a7ca4253c3d03ec0e461c5279
Shivaleela <shivaleelakatti13@gmail.com>
2023-12-05 15:45:39 +0530

    validating the user credientials by each terms provided

--- src/main/java/bridgelabzjunit/UserRegistrationJunit.java
+++ src/main/java/bridgelabzjunit/UserRegistrationJunit.java
@@ -1,71 +1,86 @@
 package bridgelabzjunit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class UserRegistrationJunit {
+public interface UserRegistrationJunit {
+	boolean validate(String input);
+}
 
-	//uc1 creating the first name with valid uppercase first letter and 3 characters//
-	public boolean validateFirstName(String firstName) {
-        boolean isValid = firstName.matches("[A-Z][a-z]{3,10}");
-        if (isValid) {
-            System.out.println("First name is valid");
-            
-        } else {
-            System.out.println("First name is not valid ");
-        }
-        return isValid;
-        }
-	
-	//uc2 creating the last name with valid uppercase first letter and 3 characters//
-	public boolean validateLastName(String lastName) {
-        boolean isValid = lastName.matches("[A-Z][a-z]{3,10}");
-        if (isValid) {
-            System.out.println("Last name is valid");
-            
-        } else {
-            System.out.println("Last name is not valid ");
-        }
-        return isValid;
-        }
-	
-	//uc3 creating the email id with valid email format//
-	//uc9 creating the pattern that clear all the email samples provided samples 
-	public boolean validateEmail_id(String email_id) {
-        boolean isValid = email_id.matches("^[A-Za-z0-9+_.-]+@gmail\\.com$");
-        if (isValid) {
-            System.out.println("Email is valid");
-            
-        } else {
-            System.out.println("Email is not valid ");
-        }
-        return isValid;
-        }
-	
-	//uc4 creating the phone num with the proper + , country code and 110 digit number//
-	public boolean validatePhone_Number(String phone_num) {
-        boolean isValid = phone_num.matches("\\+[0-9]{1,3}[0-9]{10}$");
-        if (isValid) {
-            System.out.println("Phone num is valid");
-            
-        } else {
-            System.out.println("Phone num is not valid ");
-        }
-        return isValid;
-        }
-	
-	//uc5 creating the password with min of 8 character
-	//uc6 creating password with atleast one uppercase
-	//uc7 creating password with atleast one numeric character
-	//uc8 creating password with atleast one special character
-	public boolean validatePassword(String password) {
-        boolean isValid = password.matches("(?=.*[A-Z])(?=.*[a-z\\d])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
-        if (isValid) {
-            System.out.println("Password is valid");
-            
-        } else {
-            System.out.println("Password is not valid ");
-        }
-        return isValid;
-        }
-	
+// uc1 creating the first name with valid uppercase first letter and 3
+// characters//
+class Firstname implements UserRegistrationJunit {
+	@Override
+	public boolean validate(String firstName) {
+		Pattern pattern = Pattern.compile("[A-Z][a-z]{3,}");
+		Matcher matcher = pattern.matcher(firstName);
+		boolean result = matcher.matches();
+		if (result) {
+			return true;
 
+		} else {
+			return false;
+		}
+	}
+}
+
+// uc2 creating the last name with valid uppercase first letter and 3
+// characters//
+class Lastname implements UserRegistrationJunit {
+	@Override
+	public boolean validate(String lastName) {
+		boolean isValid = lastName.matches("[A-Z][a-z]{3,}");
+		if (isValid) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+// uc3 creating the email id with valid email format//
+// uc9 creating the pattern that clear all the email samples provided samples
+class Email_id implements UserRegistrationJunit {
+	@Override
+	public boolean validate(String email_id) {
+		boolean isValid = email_id.matches("^[A-Za-z0-9+_.-]+@gmail\\.com$");
+		if (isValid) {
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+}
+
+// uc4 creating the phone num with the proper + , country code and 110 digit
+// number//
+class Phone_num implements UserRegistrationJunit {
+	@Override
+	public boolean validate(String phone_num) {
+		boolean isValid = phone_num.matches("\\+[0-9]{1,3}[0-9]{10}$");
+		if (isValid) {
+			return true;
+
+		} else {
+			return false;
+		}
+	}
+}
+
+// uc5 creating the password with min of 8 character
+// uc6 creating password with atleast one uppercase
+// uc7 creating password with atleast one numeric character
+// uc8 creating password with atleast one special character
+class Password implements UserRegistrationJunit {
+	@Override
+	public boolean validate(String password) {
+		boolean isValid = password.matches("(?=.*[A-Z])(?=.*[a-z\\d])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+		if (isValid) {
+			return true;
+
+		} else {
+			return false;
+		}
+	}
 }
--- src/test/java/bridgelabzjunit/UserRegistrationTestJunit.java
+++ src/test/java/bridgelabzjunit/UserRegistrationTestJunit.java
@@ -1,60 +1,57 @@
 package bridgelabzjunit;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserRegistrationTestJunit {
-	UserRegistrationJunit userRegistration = new UserRegistrationJunit();
 
-	  /*given_first_name_valid method is testing a first name*/
-	    @Test
-	    public void given_first_name_is_valid(){
-	        boolean isValid = userRegistration.validateFirstName("Shivaleela");
-	        boolean isNotValid = userRegistration.validateFirstName("shivaleela");
-	        Assert Assertions = null;
-	        Assertions.assertTrue(isValid);
-	        Assertions.assertFalse(isNotValid);
-	    }
-	    
-	    /*given_Last_name_valid method is testing a last name*/
-	    @Test
-	    public void given_Last_name_is_valid(){
-	        boolean isValid = userRegistration.validateLastName("Katti");
-	        boolean isNotValid = userRegistration.validateLastName("katti");
-	        Assert Assertions = null;
-	        Assertions.assertTrue(isValid);
-	        Assertions.assertFalse(isNotValid);
-	    }
-	    
-	    /*given_Email_Id_valid method is testing a email id*/
-	    @Test
-	    public void given_Email_Id_is_valid(){
-	        boolean isValid = userRegistration.validateEmail_id("shiv@gmail.com");
-	        boolean isNotValid = userRegistration.validateEmail_id("shivagmail");
-	        Assert Assertions = null;
-	        Assertions.assertTrue(isValid);
-	        Assertions.assertFalse(isNotValid);
-	    }
-	    
-	    /*given_Phone_num_valid method is testing a phone num*/
-	    @Test
-	    public void given_Phone_Num_is_valid(){
-	        boolean isValid = userRegistration.validatePhone_Number("+916361290015");
-	        boolean isNotValid = userRegistration.validatePhone_Number("6789045678");
-	        Assert Assertions = null;
-	        Assertions.assertTrue(isValid);
-	        Assertions.assertFalse(isNotValid);
-	    }
-	    
-	    /*given_Password_valid method is testing a password*/
-	    @Test
-	    public void given_Password_is_valid(){
-	        boolean isValid = userRegistration.validatePassword("Asdfghjl7@");
-	        boolean isNotValid = userRegistration.validatePassword("asdfgthi2j");
-	        Assert Assertions = null;
-	        Assertions.assertTrue(isValid);
-	        Assertions.assertFalse(isNotValid);
-	    }
+	/* given_first_name_valid method is testing a first name */
+	@Test
+	public void given_first_name_is_valid() {
+		UserRegistrationJunit userRegistration = new Firstname();
+		boolean isValid = userRegistration.validate("Shivaleela");
+		boolean isNotValid = userRegistration.validate("shivaleela");
+		assertTrue(isValid);
+		assertFalse(isNotValid);
+	}
 
+	/* given_Last_name_valid method is testing a last name */
+	@Test
+	public void given_Last_name_is_valid() {
+		UserRegistrationJunit userRegistration = new Lastname();
+		boolean isValid = userRegistration.validate("Katti");
+		boolean isNotValid = userRegistration.validate("katti");
+		assertTrue(isValid);
+		assertFalse(isNotValid);
+	}
 
+	/* given_Email_Id_valid method is testing a email id */
+	@Test
+	public void given_Email_Id_is_valid() {
+		UserRegistrationJunit userRegistration = new Email_id();
+		boolean isValid = userRegistration.validate("shiv@gmail.com");
+		boolean isNotValid = userRegistration.validate("shivagmail");
+		assertTrue(isValid);
+		assertFalse(isNotValid);
+	}
+
+	/* given_Phone_num_valid method is testing a phone num */
+	@Test
+	public void given_Phone_Num_is_valid() {
+		UserRegistrationJunit userRegistration = new Phone_num();
+		boolean isValid = userRegistration.validate("+916361290015");
+		boolean isNotValid = userRegistration.validate("6789045678");
+		assertTrue(isValid);
+		assertFalse(isNotValid);
+	}
+
+	/* given_Password_valid method is testing a password */
+	@Test
+	public void given_Password_is_valid() {
+		UserRegistrationJunit userRegistration = new Password();
+		boolean isValid = userRegistration.validate("Asdfghjl7@");
+		boolean isNotValid = userRegistration.validate("asdfgthi2j");
+		assertTrue(isValid);
+		assertFalse(isNotValid);
+	}
 }
